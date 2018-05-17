6b6054f830bc6e555566f807c7685fffdb0d33b8
lotalorafox <lotalorafox@outlook.com>
2018-05-17 08:11:07 -0500

    hashtable finished

---
+++ Hashtable.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lotalorafox
+ */
+public class Hashtable {
+    List<List<String>> data;
+    int datacan =0;
+    int e =0;
+    float fc =0; 
+    public static int prime = 109;
+    public static float primef = (float) 109.0;
+    public static int prime2 =31;
+    public Hashtable(int size){
+        data = new ArrayList<List<String>>(prime);
+        for(int i=0;i<prime;i++){
+            ArrayList<String> tem = new ArrayList<>();
+            data.add(i,tem);
+        }
+            
+    }
+    public void add(String d){
+        int code = hashcode(d);
+        data.get(code).add(d);     
+        e++;
+        this.fac();
+    }
+    public void fac(){
+        this.fc = primef/((float)e);
+    }
+    public float getfac(){
+        return fc;
+    }
+    public int find(String a){
+        int c = hashcode(a);
+        if(data.get(c).contains(a)){
+            System.out.println(c + " " +data.get(c).toString());
+            return c;
+        }
+        else
+            return -1;
+    }
+    public int weight(int c){  
+        if(c == -1){
+            return -1;
+        }
+        return data.get(c).size();
+    }
+    int hashcode(String d){
+        long n =0;
+        for(char i: d.toCharArray()){
+            n += (((int)i)*prime2);
+        }
+        n = n%prime;
+        //System.out.println("'" + d+"'" + n);
+        return (int)n;
+    }
+    public void print(){
+        int i=0;
+        for(List<String> tem:data){
+            System.out.println(i +" " +tem.toString());
+            i++;
+        }
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lotalorafox
+ */
+public class Main {
+    public static void main(String[] arcs) throws FileNotFoundException{
+        Scanner in = new Scanner(new FileReader("InsumoTallerHash.txt"));
+        Scanner sc = new Scanner(System.in,"ISO-8859-1");
+        HashSet<String> data = new HashSet();
+        System.out.print("Putting data...");
+        while(in.hasNext()){
+            System.out.print("...");
+            String[] a = in.nextLine().split(",");
+            for(String i:a){
+                data.add(i);
+            }
+        }
+        System.out.println();
+        Object[] darr = data.toArray();
+        //System.out.print(darr.length);
+        Hashtable table = new Hashtable(darr.length);
+        for(Object k: darr){
+            table.add((String)k);
+        }
+        System.out.println("Size of the table: " + table.prime);
+        System.out.println("Number of elements: " + table.e);
+        System.out.println("Charge factor: " + table.getfac());
+        //table.print();
+        System.out.println();
+        System.out.println("Put the text to find on the table, to go out put '0', to view the table put 1");
+        String s = sc.next();
+        while(!(s.equals("0")))
+        {   
+            if(!(s.equals("1"))){
+                int code = table.find(s);
+                if(code != -1)
+                    System.out.println("code " + code + " weight " + table.weight(code));
+                else
+                    System.out.println("'" + s + "' is not in the table");    
+            }else{
+                table.print();
+            }
+            s = sc.next();
+        }
+    }
+          
+}
+
